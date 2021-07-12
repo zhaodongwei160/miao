e50e763360eba0c94de4bbab11dd629bdd50790f
zhaodongwei <526850064@qq.com>
2021-07-12 17:21:22 +0800

    1

--- lodash/zhaodongwei-lodash.js
+++ lodash/zhaodongwei-lodash.js
@@ -1,10 +1,47 @@
 var zhaodongwei = function (){
-  function chunk(arr, size){
+  //chunk
+  function chunk(arr, size = 1){
+    var result = []
+    var tem = []
+    var count = 0
+    for(var i = 0; i <arr.length; i++){
+      tem.push(arr[i])
+      count++
+      if(count == size){
+        result.push(tem)
+        count = 0
+        tem = []
+      }
+    }
+    if(tem.length > 0){
+      result.push(tem)
+    }
+    return reuslt
+  }
+  //compact
+  function compact(arr){
+    return arr.filter(it => !!it === true)
+  }
+  //uniq
+  function uniq (arr){
+    var result = []
+    arr.forEach(it => {
+      if(!result.includes(it)){
+        result.push(it)
+      }
+    })
+    return result
+}
+  //uniqBy
+  function uniqBy(arr){
 
   }
 
   return {
     chunk:chunk,
+    compact:compact,
+    uniq:uniq,
+
   }
 }()
 
